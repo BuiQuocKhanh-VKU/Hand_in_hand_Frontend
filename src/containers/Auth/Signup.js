fbf45ea842f41d46e86b962272c4792763dfb48d
Khanh Bui <BuiQuocKhanh-VKU>
2025-08-11 23:52:37 +0700

    login

--- src/containers/Auth/Signup.js
+++ src/containers/Auth/Signup.js
@@ -1,4 +1,3 @@
-//file location: src/containers/Auth/Signup.js
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
@@ -19,6 +18,7 @@ class Signup extends Component {
         };
         this.listenToEmitter();
     }
+
     listenToEmitter = () => {
         emitter.on("EVENT_CLEAR_MODAL_DATA", () => {
             this.setState({
@@ -30,34 +30,37 @@ class Signup extends Component {
             });
         });
     };
+
     handleOnChangeInput = (event, id) => {
-        //good code
         let copyState = { ...this.state };
         copyState[id] = event.target.value;
         this.setState({
             ...copyState,
         });
     };
+
     checkValidateInput = () => {
         let isValid = true;
         let arrInput = ["email", "password", "phone", "name", "address"];
         for (let i = 0; i < arrInput.length; i++) {
             if (!this.state[arrInput[i]]) {
                 isValid = false;
-                alert("Missing parameter: " + arrInput[i]);
+                alert("Thiếu thông tin: " + arrInput[i]);
                 break;
             }
         }
         return isValid;
     };
+
     handleAddNewUser = () => {
         let isValid = this.checkValidateInput();
         if (isValid === true) {
             this.createNewUser(this.state);
-            alert("Create new user successfully!");
+            alert("Tạo tài khoản mới thành công!");
             this.handleNavigate("/login");
         }
     };
+
     createNewUser = async (data) => {
         try {
             let response = await createNewUserService(data);
@@ -74,111 +77,109 @@ class Signup extends Component {
             console.log("createNewUser error: ", error);
         }
     };
+
     handleNavigate = (path) => {
         this.props.history.push(path);
     };
+
     render() {
         return (
-            <div className="container mt-5 col-3">
-                <div className="main-form">
-                    <p className="title-form">Signup</p>
-                    <div className="form-group input-with-icon">
+            <div className="container mt-5 col-12 col-md-6">
+                <div className="main-form p-4 rounded shadow-lg">
+                    <h3 className="text-center mb-4">Đăng Ký</h3>
+                    <div className="form-group mb-3 input-with-icon">
                         <i className="fas fa-envelope"></i>
                         <label htmlFor="email">Email</label>
                         <input
                             type="email"
                             className="form-control"
                             name="email"
-                            placeholder="Enter your Email"
+                            placeholder="Nhập Email của bạn"
                             onChange={(event) =>
                                 this.handleOnChangeInput(event, "email")
                             }
                             value={this.state.email}
                         />
                     </div>
-                    <div className="form-group input-with-icon">
+                    <div className="form-group mb-3 input-with-icon">
                         <i className="fas fa-lock"></i>
-                        <label htmlFor="password">Password</label>
+                        <label htmlFor="password">Mật khẩu</label>
                         <input
                             type="password"
                             className="form-control"
                             name="password"
-                            placeholder="Enter your Password"
+                            placeholder="Nhập mật khẩu"
                             onChange={(event) =>
                                 this.handleOnChangeInput(event, "password")
                             }
                             value={this.state.password}
                         />
                     </div>
-                    <div className="form-group input-with-icon">
-                        <i class="fa fa-user"></i>
-                        <label htmlFor="text">Name</label>
+                    <div className="form-group mb-3 input-with-icon">
+                        <i className="fa fa-user"></i>
+                        <label htmlFor="text">Họ và tên</label>
                         <input
                             type="text"
                             className="form-control"
                             name="name"
-                            placeholder="Enter your Name"
+                            placeholder="Nhập họ và tên"
                             onChange={(event) =>
                                 this.handleOnChangeInput(event, "name")
                             }
                             value={this.state.name}
                         />
                     </div>
-                    <div className="form-group input-with-icon">
-                        <i class="fas fa-map-marker-alt"></i>
-                        <label htmlFor="address">Address</label>
+                    <div className="form-group mb-3 input-with-icon">
+                        <i className="fas fa-map-marker-alt"></i>
+                        <label htmlFor="address">Địa chỉ</label>
                         <input
                             type="text"
                             className="form-control"
                             name="address"
-                            placeholder="Enter your Address"
+                            placeholder="Nhập địa chỉ"
                             onChange={(event) =>
                                 this.handleOnChangeInput(event, "address")
                             }
                             value={this.state.address}
                         />
                     </div>
-                    <div className="form-group input-with-icon">
-                    <i class="fas fa-phone"></i>
-                        <label htmlFor="phone">Phone</label>
+                    <div className="form-group mb-4 input-with-icon">
+                        <i className="fas fa-phone"></i>
+                        <label htmlFor="phone">Số điện thoại</label>
                         <input
                             type="text"
                             className="form-control"
                             name="phone"
-                            placeholder="Enter your Phone"
+                            placeholder="Nhập số điện thoại"
                             onChange={(event) =>
                                 this.handleOnChangeInput(event, "phone")
                             }
                             value={this.state.phone}
                         />
                     </div>
-                    <div className="col-12" style={{ color: "red" }}>
-                        {this.state.errMessage}
-                    </div>
                     <button
                         type="submit"
                         className="btn btn-dark btn-block"
                         onClick={() => this.handleAddNewUser()}
-                        
                     >
-                        Signup{" "}
+                        Đăng Ký
                     </button>
+
                     <p className="text-center mt-3">
-                        Have an account?{" "}
+                        Đã có tài khoản?{" "}
                         <a onClick={() => this.handleNavigate("/login")}>
-                            Login
+                            Đăng Nhập
                         </a>
                     </p>
                     <hr />
-
-                    <button className="btn-light col-12">
+                    <button className="btn btn-light col-12">
                         <img
                             src="https://img.icons8.com/color/20/000000/google-logo.png"
                             alt="Google"
                         />
-                        Continue With Google
+                        Tiếp Tục với Google
                     </button>
-                    <button className="btn-light fb col-12">
+                    <button className="btn btn-light fb col-12">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512"
@@ -189,7 +190,7 @@ class Signup extends Component {
                                 d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"
                             />
                         </svg>
-                        Continue With Facebook
+                        Tiếp Tục với Facebook
                     </button>
                 </div>
             </div>
