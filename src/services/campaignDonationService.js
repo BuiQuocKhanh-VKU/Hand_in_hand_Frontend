a15f191b8603a47f6301de07e039c6aa216e3563
Khanh Bui <BuiQuocKhanh-VKU>
2025-08-14 23:01:16 +0700

    done services

---
+++ src/services/campaignDonationService.js
@@ -0,0 +1,46 @@
+import axios from "../axios";
+const getAllCampaignDonations = (inputId) => {
+   return axios.get(`/api/get-all-campaign-donations?id=${inputId}`);
+};
+const createNewCampaignDonationService = (data) => {
+   return axios.post("/api/create-campaign-donation", data);
+};
+
+const deleteCampaignDonationService = (id) => {
+   return axios.delete(`/api/delete-campaign-donation`, {
+      //goi phuong thuc delete
+      data: {
+         //truyen tham so vao
+         id: id, //truyen id cua campaign-donation can xoa
+      },
+   });
+};
+
+const editCampaignDonationService = (inputData) => {
+   return axios.put("/api/update-campaign-donation", inputData);
+};
+
+const getAllCampaigns = (inputId) => {
+   return axios.get(`/api/get-all-campaigns?id=${inputId}`);
+};
+const getAllUsers = (inputId) => {
+   return axios.get(`/api/get-all-users?id=${inputId}`);
+};
+
+const getCampaignDonationsByUser = (inputId) => {
+   return axios.get(`/api/get-campaign-donations-by-user?id=${inputId}`);
+};
+const getCampaignDonationsByCampaign = (inputId) => {
+   return axios.get(`/api/get-campaign-donations-by-campaign?id=${inputId}`);
+};
+
+export {
+   getAllCampaignDonations,
+   getCampaignDonationsByUser,
+   getAllUsers,
+   getAllCampaigns,
+   createNewCampaignDonationService,
+   deleteCampaignDonationService,
+   editCampaignDonationService,
+   getCampaignDonationsByCampaign,
+};
--- src/services/partnerService.js
+++ src/services/partnerService.js
@@ -1,38 +1,37 @@
 import axios from "../axios";
 const getAllPartners = (inputId) => {
-    return axios.get(`/api/get-all-partners?id=${inputId}`);
+   return axios.get(`/api/get-all-partners?id=${inputId}`);
 };
 const createNewPartnerService = (data) => {
-    return axios.post("/api/create-partner", data);
+   return axios.post("/api/create-partner", data);
 };
 
 const deletePartnerService = (partnerId) => {
-    return axios.delete(`/api/delete-partner`, {
-        //goi phuong thuc delete
-        data: {
-            //truyen tham so vao
-            id: partnerId, //truyen id cua partner can xoa
-        },
-    });
+   return axios.delete(`/api/delete-partner`, {
+      //goi phuong thuc delete
+      data: {
+         //truyen tham so vao
+         id: partnerId, //truyen id cua partner can xoa
+      },
+   });
 };
 const editPartnerService = (inputData) => {
-    return axios.put("/api/update-partner", inputData);
+   return axios.put("/api/update-partner", inputData);
 };
 
 const getAllCampaigns = (inputId) => {
-    return axios.get(`/api/get-all-campaigns?id=${inputId}`);
-}
+   return axios.get(`/api/get-all-campaigns?id=${inputId}`);
+};
 
 const getPartnerByCampaignId = (inputId) => {
-    return axios.get(`/api/get-partners-by-campaign?id=${inputId}`);
-}
-
+   return axios.get(`/api/get-partners-by-campaign?id=${inputId}`);
+};
 
 export {
-    getAllPartners,
-    createNewPartnerService,
-    deletePartnerService,
-    editPartnerService,
-    getAllCampaigns,
-    getPartnerByCampaignId
+   getAllPartners,
+   createNewPartnerService,
+   deletePartnerService,
+   editPartnerService,
+   getAllCampaigns,
+   getPartnerByCampaignId,
 };
