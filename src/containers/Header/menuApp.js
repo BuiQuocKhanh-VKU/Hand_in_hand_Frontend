83b1ba42837bee20d742278457d5c8ddd2901d12
Khanh Bui <BuiQuocKhanh-VKU>
2025-08-15 14:24:16 +0700

    statistical

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,15 +1,16 @@
 export const adminMenu = [
-  {
-    //hệ thống
-    name: "menu.admin.system",
-    menus: [
-      {
-        name: "menu.admin.manage-user",
-        link: "/system/user-manage",
-      },
-     
-
-      // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
-    ],
-  },
+   {
+      //hệ thống
+      name: "menu.admin.system",
+      menus: [
+         {
+            name: "menu.admin.statistical",
+            link: "/system/statistical",
+         },
+         {
+            name: "menu.admin.manage-user",
+            link: "/system/user-manage",
+         },
+      ],
+   },
 ];
--- src/routes/System.js
+++ src/routes/System.js
@@ -1,48 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Redirect, Route, Switch } from 'react-router-dom';
-import UserManage from '../containers/System/UserManage';
-
-
+import { Redirect, Route, Switch } from "react-router-dom";
 import { userIsAdmin } from "../hoc/authentication";
 import Header from "../containers/Header/Header";
 
+import Statistical from "../containers/System/Statistical";
+import UserManage from "../containers/System/UserManage";
+
+
+
 class System extends Component {
-    render() {
-        const { systemMenuPath, isLoggedIn } = this.props;
-        return (
-            <React.Fragment>
-                {isLoggedIn && <Header />}
-                <div className="system-container" style={{ fontFamily: "Helvetica" }}>
-                    <div className="system-list">
-                        <Switch>
-                            <Route
-                                path="/system/user-manage"
-                                component={userIsAdmin(UserManage)} // Sử dụng HOC
-                            />
-                        
-                            <Route
-                                component={() => {
-                                    return <Redirect to={systemMenuPath} />;
-                                }}
-                            />
-                        </Switch>
-                    </div>
-                </div>
-            </React.Fragment>
-        );
-    }
+   render() {
+      const { systemMenuPath, isLoggedIn } = this.props;
+      return (
+         <React.Fragment>
+            {isLoggedIn && <Header />}
+            <div className="system-container" style={{ fontFamily: "Helvetica" }}>
+               <div className="system-list">
+                  <Switch>
+                     <Route
+                        path="/system/statistical"
+                        component={userIsAdmin(Statistical)} // Sử dụng HOC
+                     />
+                     <Route
+                        path="/system/user-manage"
+                        component={userIsAdmin(UserManage)} // Sử dụng HOC
+                     />
+
+                     <Route
+                        component={() => {
+                           return <Redirect to={systemMenuPath} />;
+                        }}
+                     />
+                  </Switch>
+               </div>
+            </div>
+         </React.Fragment>
+      );
+   }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        systemMenuPath: state.app.systemMenuPath,
-        isLoggedIn: state.user.isLoggedIn,
-    };
+   return {
+      systemMenuPath: state.app.systemMenuPath,
+      isLoggedIn: state.user.isLoggedIn,
+   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+   return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(System);
